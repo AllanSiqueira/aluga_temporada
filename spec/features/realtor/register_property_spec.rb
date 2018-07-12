4af965beb0ca91cb1633a6454b034c45463ada8d
Allan Siqueira <allansiqueiraster@hotmail.com>
2018-07-12 18:13:01 -0300

    Add main photo to property

--- spec/features/realtor/register_property_spec.rb
+++ spec/features/realtor/register_property_spec.rb
@@ -9,6 +9,7 @@ feature 'Register Property' do
     click_on 'Cadastrar imóvel'
     fill_in 'Título', with: 'Lindo apartamento 100m da praia'
     fill_in 'Descrição', with: 'Um apartamento excelente para férias'
+    attach_file 'Foto destaque', Rails.root.join('spec','support','casa.jpg')
     select 'Apartamento', from: 'Tipo do imóvel'
     select 'Copacabana', from: 'Região'
     fill_in 'Finalidade do imóvel', with: 'Aluguel de Temporada'
@@ -25,6 +26,7 @@ feature 'Register Property' do
 
     expect(page).to have_css('p', text: 'Imóvel cadastrado com sucesso')
     expect(page).to have_css('h1', text: 'Lindo apartamento 100m da praia')
+    expect(page).to have_css("img[src*='casa.jpg']")
     expect(page).to have_css('p', text: 'Um apartamento excelente para férias')
     expect(page).to have_css('li', text: region.name)
     expect(page).to have_css('li', text: property_type.name)
@@ -50,6 +52,7 @@ feature 'Register Property' do
 
     expect(page).to have_content('Você deve preencher todos os campos')
     expect(page).to have_content('Title não pode ficar em branco')
+    expect(page).to have_content('Main photo não pode ficar em branco')
     expect(page).to have_content('Room quantity não pode ficar em branco')
     expect(page).to have_content('Maximum guests não pode ficar em branco')
     expect(page).to have_content('Minimum rent não pode ficar em branco')
